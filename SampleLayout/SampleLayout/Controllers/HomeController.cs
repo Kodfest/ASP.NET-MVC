002397b05172b2361ea5c3c365d2b4afc518f131
Yavuz Gedik <yavuzgedik@outlook.com>
2016-08-16 22:05:47 +0300

    Create Project

---
+++ SampleLayout/SampleLayout/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Header = "Panel";
+            return View();
+        }
+    }
+}
---
+++ SampleLayout/SampleLayout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleLayout.MvcApplication" Language="C#" %>
