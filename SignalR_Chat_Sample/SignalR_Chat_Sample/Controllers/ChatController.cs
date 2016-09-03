8ec949c13acf5a67e7a38120a45e82de367621b5
Yavuz Gedik <yavuzgedik@outlook.com>
2016-09-03 11:48:34 +0300

    Added Contoller and View

---
+++ SignalR_Chat_Sample/SignalR_Chat_Sample/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalR_Chat_Sample.Controllers
+{
+    public class ChatController : Controller
+    {
+        // GET: Chat
+        public ActionResult Friend()
+        {
+            return View();
+        }
+    }
+}
